5eae8e49a7ac9c4f84535841db294e3ddc1bb0d7
anatolk <anatol.klamer@thetrainline.com>
2018-07-10 21:37:14 +0200

    2depth tree test

--- Tree2List.Tests/Tree2ListConverterTests.cs
+++ Tree2List.Tests/Tree2ListConverterTests.cs
@@ -17,12 +17,22 @@ namespace Tree2List.Tests
         [Fact]
         public void Test1()
         {
-            var tree = new Node(
-                new Node(null, null), new Node(null, null));
+            var tree = new Node(new Node(null, null), new Node(null, null));
             LinkedList<Node> l = _subject.Convert(tree);
             l.Count.Should().Be(2);
             l.First.Value.Should().Be(tree.Left);
             l.Last.Value.Should().Be(tree.Right);
+        }
+        [Fact]
+        public void Test2()
+        {
+            var tree = new Node(new Node(new Node(null, null), new Node(null, null)), new Node(new Node(null, null), new Node(null, null)));
+            LinkedList<Node> l = _subject.Convert(tree);
+            l.Count.Should().Be(4);
+            l.First.Value.Should().Be(tree.Left.Left);
+            l.First.Next.Value.Should().Be(tree.Left.Right);
+            l.First.Next.Next.Value.Should().Be(tree.Right.Left);
+            l.First.Next.Next.Next.Value.Should().Be(tree.Right.Right);
         }
     }
 }
